33cb7d50ef7c1f18b14ee0dfad47d30eb893d36c
Christina Hedges <14965634+christinahedges@users.noreply.github.com>
2023-01-27 17:08:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="tess-point",
     version="0.8.0",
     author="Christopher J. Burke",
-    author_email="cjburke@mit.edu",
+    author_email="tesshelp@bigbang.gsfc.nasa.gov",
     description="Determine pixel coordinates for TESS targets",
     long_description=long_description,
     long_description_content_type="text/markdown",
