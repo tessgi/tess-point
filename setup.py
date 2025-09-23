3bd46b373668d82c9b48e343b12fbc11aa28a0bc
tylerapritchard <tylerapritchard@gmail.com>
2025-09-23 15:09:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tess-point",
-    version="0.9.2",
+    version="0.9.3",
     author="Christopher J. Burke",
     author_email="tesshelp@bigbang.gsfc.nasa.gov",
     description="Determine pixel coordinates for TESS targets",
